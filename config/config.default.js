9346be54d066985a63ed435cfdad4d68d7c59ed9
yangjin1215 <yangjin6483@163.com>
2020-05-04 12:21:30 +0800

    fix: fixed file upload setting

--- config/config.default.js
+++ config/config.default.js
@@ -17,7 +17,7 @@ module.exports = appInfo => {
       host: '127.0.0.1',
       port: 3306,
       username: 'root',
-      password: '',
+      password: 'root',
       database: 'vod',
       timezone: '+08:00', // 保存为本地时区,
       dialectOptions: {
@@ -35,6 +35,10 @@ module.exports = appInfo => {
       csrf: {
         headerName: 'x-csrf-token', // 通过 header 传递 CSRF token 的默认字段为 x-csrf-token
       },
+    },
+    multipart: {
+      fields: 50, // 设置文件上传字段 50个
+      fileSize: 1024 ** 3, // 设置文件上传大小 1GB
     },
   };
   config.assets = {
@@ -81,12 +85,14 @@ module.exports = appInfo => {
       videoPath: 'public/uploads/videos/',
     },
     email: {
-      host: '',
-      name: '猿学网',
-      pass: '',
-      feedback: '',
+      host: '', // 反馈邮件的发件人邮箱 【你可以写自己的QQ邮箱，或者其他的邮箱】
+      name: '猿学网', // 反馈邮件的发件人名称
+      pass: '', // 反馈邮件的发件人邮箱 授权码（不是密码） 【需要你填写授权码】
+      feedback: '', // 反馈邮件的收件人邮箱
     },
   };
+
+  // 此文件如果要提交的话, 一定要先把 email 下的敏感信息删除后再提交
 
   return {
     ...config,
